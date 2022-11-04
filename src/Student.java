446c051d2cf79a7cbb9b740c0079216c7e5da1ba
Sanitar52 <mert_ozincegedik@hotmail.com>
2022-11-04 17:17:41 +0300

    Added necessary class setter and getter methods for Student class.

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,6 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Hashtable;
+
 public class Student {
     int studentID;
     String fName;
@@ -24,27 +23,56 @@ public class Student {
         this.gmailAddress = gmailAddress;
 
     }
-    void changeStudentID(int id){
+    public void changeStudentID(int id){
         this.studentID = id;
     }
+    int getStudentID() { return this.studentID; }
 
-    void changefName(String firstName){
+    public void changefName(String firstName){
         this.fName=firstName;
     }
 
-    void changelName(String lastName){
+    String getfName() { return this.fName; }
+
+    public void changelName(String lastName){
         this.lName = lastName;
     }
+    String getlName() { return this.lName; }
 
-    void changeGpa(int gpa){
+    public void changeGpa(int gpa){
         this.GPA = gpa;
     }
+    int getGPA() { return this.GPA; }
 
-    void changeCourseGrade(String courseGrade){
+    public void changeCourseGrade(String courseGrade){
         this.courseGrade=courseGrade;
     }
+    String getCourseGrade() { return this.courseGrade;}
 
-    void changeGmailAddress(String gmailAddress){
+    public void changeCurrentSelectedCourses(List<String>currentCourses){ this.currentSelectedCourses = currentCourses; }
+
+    List<String> getCurrentSelectedCourses() { return this.currentSelectedCourses; }
+
+    public void changeCompletedCourses(List<String>completedCourses){ this.completedCourses = completedCourses; }
+    List<String> getCompletedCourses() { return this.completedCourses; }
+    public void changeMandatoryCourses(List<String>mandatoryCourses){ this.mandatoryCourses = mandatoryCourses; }
+    List<String> getMandatoryCourses() { return this.mandatoryCourses; }
+    public void changeGmailAddress(String gmailAddress){
         this.gmailAddress = gmailAddress;
+    }
+    String getGmailAddress() { return this.gmailAddress; }
+
+    void printStudentInfo(){
+        System.out.println( "Student ID: " +this.studentID+ "\nName: " +this.fName+ " " +this.lName+ "\nGPA: " +this.GPA+ "\nEmail Address: " +this.gmailAddress+ "\nCurrent Selected Courses: " +this.currentSelectedCourses+ "\nCompleted Courses: " +this.completedCourses+ "\nCourses Needed to Finish to Graduate: " +this.mandatoryCourses);
+    }
+    public static void main(String args[]){
+        List<String> Test = new ArrayList<>();
+        Test.add("1");
+        Test.add("2");
+
+        Student newS = new Student(123, "Mert", "Özincegedik", 3, "A",Test,Test,Test,"gmail.com");
+
+        newS.printStudentInfo();
+
     }
 }
