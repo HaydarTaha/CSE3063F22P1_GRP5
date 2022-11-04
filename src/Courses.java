9c9d647ad1ad541de512739a4d61dc19d0e2d473
Burak Dursun <burakdursunn.bd@gmail.com>
2022-11-04 20:35:02 +0300

    4 variable(name, courseCode, prerequisite, credit) added in Courses class

--- src/Courses.java
+++ src/Courses.java
@@ -1,5 +1,50 @@
 public class Courses {
+
+    private String name;
+    private String courseCode;
+    private String prerequisite;
+    private int credit;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getPrerequisite() {
+        return prerequisite;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public void setPrerequisite(String prerequisite) {
+        this.prerequisite = prerequisite;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
     public Courses(){
 
+
+
+
+
+
+
     }
+
 }
