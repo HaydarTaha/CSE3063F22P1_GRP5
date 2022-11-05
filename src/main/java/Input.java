c948546e954a00e29465f83445278b2681779bef
Haydar Taha Tunç <tahatunc163@gmail.com>
2022-11-05 19:49:58 +0300

    Added studentNames.json

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -9,11 +9,11 @@ public class Input {
         try {
             Object obj = parser.parse(new FileReader(".\\\\inputs\\\\students.json"));
             JSONObject jsonObject = (JSONObject) obj;
-            String studentid =  (String) jsonObject.get("studentId");
+            String studentId =  (String) jsonObject.get("studentId");
             String fname = (String) jsonObject.get("fName");
             String lname = (String) jsonObject.get("lName");
 
-            System.out.println("Student Id:" + studentid);
+            System.out.println("Student Id:" + studentId);
             System.out.println("First Name:" + fname);
             System.out.println("Last Name:" + lname);
 
