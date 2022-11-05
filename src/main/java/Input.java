806a0b99cb8cccfaa00f48cdbf7ef6d81b49d903
emire <emirege280@gmail.com>
2022-11-05 20:08:50 +0300

    Updated Input.java

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -6,7 +6,7 @@ public class Input {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader(".\\inputs\\lectures.json"));
+            Object obj = parser.parse(new FileReader(".\\\\inputs\\\\lectures.json"));
             JSONObject jsonObject = (JSONObject) obj;
             //String studentid =  (String) jsonObject.get("studentId");
             //String fname = (String) jsonObject.get("fName");
@@ -15,7 +15,6 @@ public class Input {
             //JSONArray fname = (JSONArray)jsonObject.get("fName");
             //JSONArray lname = (JSONArray)jsonObject.get("lName");
             System.out.println("Lectures" + lectures);
-
             //System.out.println("First Name:" + fname);
             //System.out.println("Last Name:" + lname);
 
