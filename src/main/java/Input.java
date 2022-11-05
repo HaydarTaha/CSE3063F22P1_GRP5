be1240d8a76c53c03a3cf5c219677d3191b617ae
emire <emirege280@gmail.com>
2022-11-05 17:17:41 +0300

    Updated Input.java

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,4 +1,4 @@
-/*
+
 import java.io.*;
 import java.util.*;
 import org.json.simple.*;
@@ -7,8 +7,16 @@ public class Input {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader("/inputs/ lectures.json"));
-            JSONObject jsonObject = (JSONObject)obj;
+            Object obj = parser.parse(new FileReader(".\\\\inputs\\\\students.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            String studentid =  (String) jsonObject.get("studentId");
+            String fname = (String) jsonObject.get("fName");
+            String lname = (String) jsonObject.get("lName");
+
+            System.out.println("Student Id:" + studentid);
+            System.out.println("First Name:" + fname);
+            System.out.println("Last Name:" + lname);
+
 
 
         } catch (Exception e){
@@ -18,4 +26,3 @@ public class Input {
 
     }
 }
-*/
