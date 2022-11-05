1298327041c07bc7610845a3db9c03def8349fef
emire <emirege280@gmail.com>
2022-11-05 19:54:39 +0300

    Updated Input.java

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 import org.json.simple.*;
@@ -7,15 +6,24 @@ public class Input {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader(".\\\\inputs\\\\students.json"));
+            Object obj = parser.parse(new FileReader(".\\inputs\\lectures.json"));
             JSONObject jsonObject = (JSONObject) obj;
-            String studentid =  (String) jsonObject.get("studentId");
-            String fname = (String) jsonObject.get("fName");
-            String lname = (String) jsonObject.get("lName");
+            //String studentid =  (String) jsonObject.get("studentId");
+            //String fname = (String) jsonObject.get("fName");
+            //String lname = (String) jsonObject.get("lName");
+            JSONArray lectures = (JSONArray)jsonObject.get("lectures");
+            //JSONArray fname = (JSONArray)jsonObject.get("fName");
+            //JSONArray lname = (JSONArray)jsonObject.get("lName");
+            System.out.println("Lectures" + lectures);
 
-            System.out.println("Student Id:" + studentid);
-            System.out.println("First Name:" + fname);
-            System.out.println("Last Name:" + lname);
+            //System.out.println("First Name:" + fname);
+            //System.out.println("Last Name:" + lname);
+
+            Iterator<String> iterator = lectures.iterator();
+
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
 
 
 
