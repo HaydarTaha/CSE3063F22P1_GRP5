542780f28a63ff76e550186d4ab8a1d0dfe7ce77
Burak Dursun <burakdursunn.bd@gmail.com>
2022-11-12 21:22:39 +0300

    Created TranscriptCreator class and some methods added.

---
+++ src/main/java/TranscriptCreator.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class TranscriptCreator {
+
+    public Transcript createTranscript(){
+
+        return null;
+    }
+    public List<CompletedCourses> takeCompletedCourses(){
+
+        return null;
+    }
+    public List<CompletedCourses> takeFailedCourses(){
+
+        return null;
+    }
+    public double calculateGPA(){
+
+        return 0;
+    }
+    public int calculateTotalCredits(){
+
+        return 0;
+    }
+}
