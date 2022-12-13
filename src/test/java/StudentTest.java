02bf03e8740e9e034ec0d198b172447287609a53
emire <emirege280@gmail.com>
2022-12-13 19:44:45 +0300

    I wrote Unit Testing StudentTest.java

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,31 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    Student studentTest = new Student();
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    File studentJson = new File("inputs/studentNames.json");
+    Student[] studentArray = objectMapper.readValue(studentJson, Student[].class);
+
+    StudentTest() throws IOException, IOException {
+    }
+
+    @Test
+    void getfName() {
+        String expect = "NİLAY";
+        assertEquals(expect,studentArray[0].getfName());
+    }
+
+    @Test
+    void getlName() {
+        String expect = "AKSOY";
+        assertEquals(expect,studentArray[0].getlName());
+    }
+}
