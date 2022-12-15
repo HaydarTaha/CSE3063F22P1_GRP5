24bd9e413d9d5809e051f794790f0f21f15f033d
haldemir <kerem_kolay@hotmail.com>
2022-12-15 23:31:40 +0300

    wrote unit tests for CoursesTest.java

--- src/test/java/CoursesTest.java
+++ src/test/java/CoursesTest.java
@@ -17,19 +17,61 @@ class CoursesTest  {
     CoursesTest() throws IOException {
     }
 
+    // I choose 10. element as test object because it is one of the ones whose prerequisite is filled
+    // TODO: Sort test functions as main func
+
     @Test
     void testGetName() {
-        String expect = "Calculus I";
-        assertEquals(expect,coursesArray[0].getName());
+        String expect = "Computer Programming II";
+        assertEquals(expect,coursesArray[10].getName());
     }
 
     @Test
     void testGetCourseCode() {
-        String expectedCourseCode="MATH1001";
-        assertEquals(expectedCourseCode,coursesArray[0].getCourseCode());
+        String expectedCourseCode="CSE1242";
+        assertEquals(expectedCourseCode,coursesArray[10].getCourseCode());
     }
 
     @Test
-    void testGetCredit() {
+    void getSemester() {
+        int expectedSemester = 2;
+        assertEquals(expectedSemester,coursesArray[10].getSemester());
     }
+    @Test
+    void getCourseYear() {
+
+        int expectedCourseYear = 1;
+        assertEquals(expectedCourseYear,coursesArray[10].getCourseYear());
+    }
+    @Test
+    void getTheoreticalCourseHour() {
+        int expectedTheoreticalCourseHour = 3;
+        assertEquals(expectedTheoreticalCourseHour,coursesArray[10].getTheoreticalCourseHour());
+    }
+    @Test
+    void getPracticalLessonHour() {
+        int expectedPracticalLessonHour = 2;
+        assertEquals(expectedPracticalLessonHour,coursesArray[10].getPracticalLessonHour());
+    }
+    @Test
+    void getCourseType() {
+        int expectedCourseType = 5;
+        assertEquals(expectedCourseType,coursesArray[10].getCourseType());
+    }
+    @Test
+    void getPrerequisite() {
+        // will be edited later
+
+    }
+    @Test
+    void getCredit() {
+        int expectedCredit = 6;
+        assertEquals(expectedCredit,coursesArray[10].getCredit());
+    }
+    @Test
+    void getPreRequisiteName() {
+        // will be edited later
+
+    }
+
 }
