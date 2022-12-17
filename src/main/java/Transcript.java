eebb3fd10a2b28f9a6ac8f4c3c88a1538b7098ba
osama-getbaraka <osama@getbaraka.com>
2022-12-17 17:23:36 +0300

    Refactoring redundant code

--- src/main/java/Transcript.java
+++ src/main/java/Transcript.java
@@ -15,7 +15,6 @@ public class Transcript extends Student {
     private Double gpa;
     private int completedCredits;
     private Student student;
-    private Advisor studentAdvisor;
     private List<String> studentSelectedCourses;
     private List<String> completedCourseStrings;
     private List<String> failedCoursesStrings;
@@ -29,7 +28,6 @@ public class Transcript extends Student {
         this.completedCredits = completedCredits;
         this.studentSelectedCourses = studentSelectedCourses;
         this.student = student;
-        this.studentAdvisor = student.getAdvisor();
 
     }
 
