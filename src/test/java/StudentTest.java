2092be04c179789ebbefe2f8a9ffaa68de24f44f
Emirovic <devranemir08@gmail.com>
2022-12-17 07:57:43 +0300

    wrote unit tests on StudentTest.java

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -28,4 +28,41 @@ class StudentTest {
         String expect = "AKSOY";
         assertEquals(expect,studentArray[0].getlName());
     }
+    @Test
+    void testSetfName(){
+        Student studentTest = new Student();
+        studentTest.setfName("MAHMUT");
+        assertEquals("MAHMUT",studentTest.getfName());
+    }
+
+    @Test
+    void testSetlName(){
+        Student studentTest = new Student();
+        studentTest.setlName("TUNCER");
+        assertEquals("TUNCER",studentTest.getlName());
+    }
+
+    @Test
+    void testSetCurrentYear() {
+        Student studentTest = new Student();
+        studentTest.setCurrentYear(0);
+        assertEquals(0,studentTest.getCurrentYear());
+    }
+
+    @Test
+    void testSetCurrentSemester() {
+        Student studentTest = new Student();
+        studentTest.setCurrentSemester(3);
+        assertEquals(3,studentTest.getCurrentSemester());
+    }
+
+    //will be written later
+    @Test
+    void testGetTranscript() {
+        Student studentTest = new Student();
+        String expectedTranscript = "2.75";
+        //assertEquals(expectedTranscript,studentTest.getCurrentSemester());
+    }
+
+
 }
