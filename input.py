1b164ee8018b86b246250aafee29989207b166ba
Haydar Taha Tunç <tahatunc163@gmail.com>
2023-01-04 00:15:48 +0300

    Updated input.py and main.py

--- input.py
+++ input.py
@@ -4,12 +4,11 @@ from courses import Courses
 
 class Input:
     def __init__(self, data):
-        self.courses = None
-        self.nte = None
-        self.fte = None
-        self.ue = None
-        self.fte = None
-        self.te = None
+        self.courses = []
+        self.nte = []
+        self.fte = []
+        self.ue = []
+        self.te = []
         self.semester = data['semester']
         self.courseFFRate = data['courseFFRate']
         self.quotaForElectives = data['quotaForElectives']
@@ -23,7 +22,32 @@ class Input:
         self.advisorsJsonName = data['advisorsJsonName']
         self.studentsJsonName = data['studentsJsonName']
 
-    def create_objects(self):
+    def create_courses(self):
         with open(self.coursesJsonName, 'r') as f:
             coursesData = json.load(f)
-        self.courses = Courses(coursesData)
+        for data in coursesData:
+            tempCourse = Courses(data)
+            self.courses.append(tempCourse)
+        with open(self.electiveNTEJsonFileName, 'r') as f:
+            nteData = json.load(f)
+        for data in nteData:
+            tempCourse = Courses(data)
+            self.nte.append(tempCourse)
+        with open(self.electiveFTEJsonFileName, 'r') as f:
+            fteData = json.load(f)
+        for data in fteData:
+            tempCourse = Courses(data)
+            self.fte.append(tempCourse)
+        with open(self.electiveUEJsonFileName, 'r') as f:
+            ueData = json.load(f)
+        for data in ueData:
+            tempCourse = Courses(data)
+            self.ue.append(tempCourse)
+        with open(self.electiveTEJsonFileName, 'r') as f:
+            teData = json.load(f)
+        for data in ueData:
+            tempCourse = Courses(data)
+            self.te.append(tempCourse)
+
+    def create_objects(self):
+        self.create_courses()
