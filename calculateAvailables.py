69b55bd779c3cdcdba98f544520c5b67a1af4668
mehmetemresenguler <mehmetemresenguler.01@gmail.com>
2023-01-04 18:50:07 +0300

    some methods updated to Python codecalculateAvailables.py

--- calculateAvailables.py
+++ calculateAvailables.py
@@ -29,3 +29,53 @@ class CalculateAvailables:
 
     def get_max_number_of_selection_for_courses(self) -> int:
         return self.max_number_of_selection_for_courses
+
+    def set_attributes(courses):
+        # Everytime i call this method i need to update these lists again
+        semester_one_courses_names = []
+        semester_two_courses_names = []
+        semester_three_courses_names = []
+        semester_four_courses_names = []
+        semester_five_courses_names = []
+        semester_six_courses_names = []
+        semester_seven_courses_names = []
+        semester_eight_courses_names = []
+
+        for course in courses:
+            if course.get_semester() == 1:
+                semester_one_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 2:
+                semester_two_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 3:
+                semester_three_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 4:
+                semester_four_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 5:
+                semester_five_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 6:
+                semester_six_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 7:
+                semester_seven_courses_names.append(course.get_course_code())
+            elif course.get_semester() == 8:
+                semester_eight_courses_names.append(course.get_course_code())
+
+    def put_available_courses_case_two(courses, student_courses_took, std_name):
+        calculated_semester_two_course_names = []
+        for i in range(0, len(student_courses_took), 2):
+            course_name = student_courses_took[i]
+            course_grade = student_courses_took[i + 1]
+            if course_grade == "FF":
+                test = False
+                # Here we iterate every courses available until we find the same course with the completed one of the student.
+                for courses1 in courses:
+                    # If a failed course is a prerequisite to another course
+                    # We delete that course in calculated list and add the failed course back to the calculatedlist
+                    if course_name == courses1.get_prerequisite_name():
+                        prerequisite = courses1.get_prerequisite_name()
+                        print(f"{std_name} Failed: {prerequisite} He cannot choose {courses1.get_course_code()}")
+                        calculated_semester_two_course_names.remove(courses1.get_course_code())
+                        calculated_semester_two_course_names.add(prerequisite)
+                        test = True
+                        break
+                # If he failed but there is not any prerequisite needed for above courses
+                # we add the same course again but we do not remove anything.
